fd887b5caf130987b30d0bc8da9b97e58aef3e14
Sam Laane <laane2@acm.uiuc.edu>
2013-09-30 19:35:20 -0500

    area code

--- arena/qualifier.py
+++ arena/qualifier.py
@@ -1,9 +1,25 @@
+#!/usr/bin/env python
 import os
 import mm19_runner
-#path, filename = os.path.split(os.path.abspath(__file__))
-path = r"/home/more/safe/sam/project/acm/mm19/arena"
+import signal
+import json
+
+path, filename = os.path.split(os.path.abspath(__file__))
+#path = r"/home/more/safe/sam/project/acm/mm19/arena"
 path += "/teams"
 teams = {}
+OUTPUT_NAME = "qualified_teams.txt"
+TIMEOUT = 120  # two minute timeout
+
+
+class Alarm(Exception):
+    pass
+
+
+def alarm_handler(signum, frame):
+    raise Alarm
+
+signal.signal(signal.SIGALRM, alarm_handler)
 
 
 def add_team(team, name_file, run_file):
@@ -15,23 +31,37 @@ def add_team(team, name_file, run_file):
         return
     if qualify(name, run_file):
         teams[name] = run_file
-    else: 
+    else:
         print name+" failed to qualify"
 
-def qualify(name, run_file): 
-    return mm19_runner.testGame(name, run_file)
 
-for dir_entry in os.listdir(path):
-    dir_entry_path = os.path.join(path, dir_entry)
-    files = os.listdir(dir_entry_path)
-    if "name.txt" in files:
-        if "run.sh" in files:
-            add_team(dir_entry,
-                     os.path.join(dir_entry_path,"name.txt"),
-                     os.path.join(dir_entry_path, "run.sh"))
-        else: 
-            print "error missing run.sh for entry "+dir_entry
-    else:
-        print "error missing name.text for entry "+dir_entry
+def qualify(name, run_file):
+    signal.alarm(TIMEOUT)  # set timeout
+    try:
+        result = mm19_runner.testGame(name, run_file)
+        signal.alarm(0)  # cancel alarm
+        return result
+    except Alarm:
+        print name + " timed out"
+        return False
 
-print teams
+def main():
+    for dir_entry in os.listdir(path):
+        dir_entry_path = os.path.join(path, dir_entry)
+        files = os.listdir(dir_entry_path)
+        if "name.txt" in files:
+            if "run.sh" in files:
+                add_team(dir_entry,
+                         os.path.join(dir_entry_path, "name.txt"),
+                         os.path.join(dir_entry_path, "run.sh"))
+            else:
+                print "error missing run.sh for entry "+dir_entry
+        else:
+            print "error missing name.text for entry "+dir_entry
+            
+    print teams
+    with open(OUTPUT_NAME, 'w') as f:
+        f.write(json.dumps(teams))
+
+if __name__ == "__main__":
+    main()
