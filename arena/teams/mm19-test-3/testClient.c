3b32552719b622b514f41287f8852758c1e7f76f
Sam Laane <laane2@acm.uiuc.edu>
2013-10-08 23:44:24 -0500

    bugfix

--- arena/teams/mm19-test-3/testClient.c
+++ arena/teams/mm19-test-3/testClient.c
@@ -1,5 +1,7 @@
 /*
-** listener.c -- a datagram sockets "server" demo
+** testClient for mm19!! In C!
+**
+** The purpose of this class is to do the communications with the servwer, and parse the json into a form that we can use.
 */
 
 #include <stdio.h>
@@ -12,25 +14,36 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <netdb.h>
+#include "clientAI.c"
 
-#define MAXDATASIZE 8192 //Probably not a useful number for our purposes...
+
 #define HOST "localhost"
 #define PORT "6969"
 
-typedef struct{
-	char type;
-	int x;
-	int y;
-	int health;
-	int ID;
-	char orient;
-} ship;
 
-typedef struct{
-	int x;
-	int y;
-	int success;
-} hit;
+char *loadFile(char *filename)
+{
+	FILE* input = fopen(filename, "rb");
+  if(input == NULL) return NULL;
+ 
+  if(fseek(input, 0, SEEK_END) == -1) return NULL;
+  long size = ftell(input);
+  if(size == -1) return NULL;
+  if(fseek(input, 0, SEEK_SET) == -1) return NULL;
+ 
+  char *content =malloc( (size_t) size +1  ); 
+  if(content == NULL) return NULL;
+ 
+  fread(content, 1, (size_t)size, input);
+  if(ferror(input)) {
+    free(content);
+    return NULL;
+  }
+ 
+  fclose(input);
+  content[size] = '\0';
+  return content;
+}
 
 //Credit to Beej's Guide
 int sendall(int s, char *buf, int *len)
@@ -59,93 +72,6 @@ void *get_in_addr(struct sockaddr *sa)
 	}
 
 	return &(((struct sockaddr_in6*)sa)->sin6_addr);
-}
-
-//TODO: Use strlen to determine size of strings...
-
-char * makeShip(char type, int x, int y, char orient){
-	char * buf = malloc(70);
-	char * temp=buf;
-	if (type=='M'){
-		sprintf(temp, "\"mainShip\" : { "); //print to string
-		temp=temp+15; //Ew, yucky constants!
-	} else {
-		sprintf(temp, "{ \"type\" : \"%c\", ", type);
-		temp=temp+16;
-	}
-	sprintf(temp, "\"xCoord\" : %d, \"yCoord\" : %d, \"orientation\" : \"%c\" }", x, y, orient);
-	
-	return buf;
-}
-
-char * init(char * name){
-	char * buf= malloc(MAXDATASIZE);
-	char * ship1 = makeShip('M', 5, 0, 'H'); //ship1 must always be main ship!!
-	char * ship2 = makeShip('P', 6, 6, 'H');
-	char * ship3 = makeShip('P', 8, 8, 'H');
-	char * ship4 = makeShip('P', 10, 10, 'H');
-	char * ship5 = makeShip('P', 12, 12, 'H');
-	char * ship6 = makeShip('P', 14, 14, 'H');
-	char * ship7 = makeShip('P', 16, 16, 'H');
-	char * ship8 = makeShip('P', 18, 17, 'H');
-	char * ship9 = makeShip('P', 20, 18, 'H');
-	char * ship10 = makeShip('P', 22, 20, 'H');
-	char * ship11 = makeShip('P', 24, 30, 'H');
-	char * ship12 = makeShip('D', 26, 34, 'H');
-	char * ship13 = makeShip('D', 28, 56, 'V');
-	char * ship14 = makeShip('D', 30, 78, 'H');
-	char * ship15 = makeShip('D', 32, 4, 'H');
-	char * ship16 = makeShip('D', 34, 34, 'H');
-	char * ship17 = makeShip('D', 36, 36, 'H');
-	char * ship18 = makeShip('D', 38, 38, 'H');
-	char * ship19 = makeShip('D', 40, 40, 'H');
-	//sprintf(buf, "{ \"playerName\" : \"Roger\", \"mainShip\" : { \"xCoord\" : 5, \"yCoord\" : 5, \"orientation\" : \"H\" } , \"ships\" : [ 	{  \"type\" : \"D\", \"xCoord\" : 10,\"yCoord\" : 10, \"orientation\" : \"H\" },{  \"type\" : \"P\", \"xCoord\" : 20, \"yCoord\" : 20, \"orientation\" : \"V\" }, {  \"type\" : \"D\", \"xCoord\" : 30, \"yCoord\" : 30, \"orientation\" : \"V\" }, {  \"type\" : \"P\", \"xCoord\" : 40, \"yCoord\" : 40, \"orientation\" : \"H\" } ] }\n");
-	sprintf(buf, "{ \"playerName\" : \"%s\", %s, \"ships\" : [ %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s ] }\n", name, ship1, ship2, ship3, ship4, ship5, ship6, ship7, ship8, ship9, ship10, ship11, ship12, ship13, ship14, ship15, ship16, ship17, ship18, ship19);
-	free(ship1);
-	free(ship2);
-	free(ship3);
-	free(ship4);
-	free(ship5);
-	free(ship6);
-	free(ship7);
-	free(ship8);
-	free(ship9);
-	free(ship10);
-	free(ship11);
-	free(ship12);
-	free(ship13);
-	free(ship14);
-	free(ship15);
-	free(ship16);
-	free(ship17);
-	free(ship18);
-	free(ship19);
-	return buf;
-}
-
-//Returns 0 to continue, 1 to quit, 2 to send
-int getTurn(char * buf,  char * token, int response, int resources, ship * ships, char ** errors, hit * hits){
-	if(response==-1 || response==9001) {
-		freeAll(ships, errors, hits);
-		return 1;
-	}
-	if(response!=100) {
-		freeAll(ships, errors, hits);
-		return 0;
-	}
-	sprintf(buf, "{ \"playerToken\" : \"qwerty123\", \"shipActions\" : [ { \"ID\" : 0, \"actionID\" : \"F\", \"actionX\" : 5, \"actionY\" : 0, \"actionExtra\" : 0 } ] }\n");
-	freeAll(ships, errors, hits);
-	return 2;
-}
-
-int freeAll(ship * ships, char ** errors, hit * hits){
-	free(ships);
-	free(hits);
-	int i=0;
-	for(i=0; errors[i]!=0; i++){
-		free(errors[i]);
-	}
-	free(errors);
 }
 
 int responseCode(char * str){
@@ -180,7 +106,6 @@ ship * ships(char * str){
 		ships[i].y=atoi(strstr(strstr(temp, "yCoord"), ":")+1);
 		ships[i].type=strstr(strstr(strstr(temp, "type"), ":"), "\"")[1];
 		ships[i].health=atoi(strstr(strstr(temp, "health"), ":")+1);
-		printf("health=%d\n", ships[i].health);
 		temp=strstr(temp, "{")+1;
 	}
 	ships[end].ID=-1;
@@ -216,13 +141,80 @@ char ** errors(char * str){
 }
 
 hit * hits(char * str){
-	return 0;
+	char * temp=strstr(strstr(str, "hitReport"), "[");
+	char * tempend=strstr(temp, "]");
+	temp=strstr(temp, "{");
+	int i=0;
+	while(tempend>temp && temp>str){
+		temp=strstr(temp+1, "{");
+		i++;
+	}
+
+	hit * hits=malloc(sizeof(hit)*(i+1));
+	temp=strstr(strstr(strstr(str, "hitReport"), "["), "{")+1;
+	int end=i;
+	for(i=0; i<end; i++){
+		hits[i].x=atoi(strstr(strstr(temp, "xCoord"), ":")+1);
+		hits[i].y=atoi(strstr(strstr(temp, "yCoord"), ":")+1);
+		if(strstr(strstr(temp, "hit"), ":")[1] == 't') hits[i].success=1;
+		else hits[i].success=0;
+		temp=strstr(temp, "{")+1;
+	}
+	hits[end].success=-1;
+
+	return hits;
+}
+
+ping * pings(char * str){
+	char * temp=strstr(strstr(str, "pingReport"), "[");
+	char * tempend=strstr(temp, "]");
+	temp=strstr(temp, "{");
+	int i=0;
+	while(tempend>temp && temp>str){
+		temp=strstr(temp+1, "{");
+		i++;
+	}
+
+	ping * pings=malloc(sizeof(ping)*(i+1));
+	temp=strstr(strstr(strstr(str, "pingReport"), "[")+1, "{")+1;
+	int end=i;
+	for(i=0; i<end; i++){
+		pings[i].ID=atoi(strstr(strstr(temp, "ID"), ":")+1);
+		pings[i].dist=atoi(strstr(strstr(temp, "distance"), ":")+1);
+		temp=strstr(temp, "{")+1;
+	}
+	pings[end].ID=-1;
+
+	return pings;
+}
+
+actionResponse * actionResponses(char * str){
+	char * temp=strstr(strstr(str, "shipActionResults"), "[");
+	char * tempend=strstr(temp, "]");
+	temp=strstr(temp, "{");
+	int i=0;
+	while(tempend>temp && temp>str){
+		temp=strstr(temp+1, "{");
+		i++;
+	}
+
+	actionResponse * responses=malloc(sizeof(actionResponse)*(i+1));
+	temp=strstr(strstr(strstr(str, "shipActionResults"), "[")+1, "{")+1;
+	int end=i;
+	for(i=0; i<end; i++){
+		responses[i].ID=atoi(strstr(strstr(temp, "ID"), ":")+1);
+		responses[i].result=strstr(strstr(strstr(temp, "result"), ":"), "\"")[1];
+		temp=strstr(temp, "{")+1;
+	}
+	responses[end].ID=-1;
+
+	return responses;
 }
 
 int main(void)
 {
-	char * buf= init("Roger"); //get starting message
-	printf("%s\n", buf);
+	char * name=loadFile("name.txt");
+	char * buf= init(name); //get starting message
 	int sockfd, numbytes;
 	struct addrinfo hints, *servinfo, *p;
 	int rv;
@@ -261,13 +253,10 @@ int main(void)
 
 	inet_ntop(p->ai_family, get_in_addr((struct sockaddr *)p->ai_addr),
 			s, sizeof s);
-	printf("client: connecting to %s\n", s);
 
 	freeaddrinfo(servinfo); // all done with this structure
 
 	send(sockfd, buf, strlen(buf)+1, 0);
-
-	printf("sent data\n");
 
 	char str[MAXDATASIZE];
 	if ((numbytes = recv(sockfd, str, MAXDATASIZE-1, 0)) == -1) {
@@ -276,8 +265,6 @@ int main(void)
 	}
 
 	str[numbytes] = '\0';
-
-	printf("client: received '%s', %d bytes\n",str, numbytes);
 
 	//Determine my player token
 	char * temptok = strstr(strstr(strstr(str, "playerToken"), ":"), "\"")+1;
@@ -289,11 +276,10 @@ int main(void)
 	int res;
 
 	while(1){
-		res=getTurn(buf, token, responseCode(str), resources(str), ships(str), errors(str), hits(str));
+		res=getTurn(buf, token, responseCode(str), resources(str), ships(str), errors(str), hits(str), actionResponses(str), pings(str));
 		if(res==1) break;
 		if(res==2) {
 			send(sockfd, buf, strlen(buf)+1, 0);
-			printf("sent data\n");
 		}
 
 		if ((numbytes = recv(sockfd, str, MAXDATASIZE-1, 0)) == -1) {
@@ -302,13 +288,12 @@ int main(void)
 		}
 
 		str[numbytes] = '\0';
-
-		printf("client: received '%s', %d bytes\n",str, numbytes);
 	}
 
 	close(sockfd);
 	free(buf);
 	free(token);
+	free(name);
 
 	return 0;
 }
--- arena/webserver.py
+++ arena/webserver.py
@@ -10,15 +10,21 @@ import time
 import threading
 
 GameThread = {}
-
 ARENA_VISUALIZER_FILE = "current_match.tmp"
-
 teams = {}
+
+HOW_OFTEN_TO_PULL_IN_MIN = 60 # update every hour
+
+TIME_SINCE_OF_PULL = 0;
+
 #def qualifyed_teams(): 
 #    return { 'Roger': '/home/more/safe/sam/project/acm/mm19/arena/teams/mm19-test-3', 'oneFish': '/home/more/safe/sam/project/acm/mm19/arena/teams/team1'}
 
 path, filename = os.path.split(os.path.abspath(__file__))
 path += "/teams"
+
+def time_to_update():
+    return (time.time() - TIME_SINCE_OF_PULL > HOW_OFTEN_TO_PULL_IN_MIN * 60) 
 
 # makes sure only the visualizer will be found
 class mmRequstHandler(SimpleHTTPRequestHandler,object):
@@ -33,7 +39,6 @@ class mmRequstHandler(SimpleHTTPRequestHandler,object):
                     self.send_header("display_text","Exhibition Match")
                     self.end_headers()
                     self.wfile.write(f.read())
-                    print self.close_connection
                 else:
                     self.send_error(500)
                 GameThread = threading.Thread(target=play_game)
@@ -45,6 +50,7 @@ class mmRequstHandler(SimpleHTTPRequestHandler,object):
 
 
 def update_repos():
+    print "____________________updating___________________"
     for dir_entry in os.listdir(path):
         dir_entry_path = os.path.join(path, dir_entry)
         files = os.listdir(dir_entry_path)
@@ -52,9 +58,13 @@ def update_repos():
             git_pull(dir_entry_path)
         else:
             print dir_entry_path+" does not have a git repo"
-        
+    TIME_SINCE_OF_PULL= time.time()
+            
 
 def play_game():
+    if time_to_update:
+        update_repos()
+        qualify()
     bot1,bot2 = random.sample(teams,2)
     runGame(ARENA_VISUALIZER_FILE, bot1, bot2, teams[bot1], teams[bot2])
     
@@ -77,14 +87,16 @@ def start_web_server():
     print "Serving HTTP on", sa[0], "port", sa[1], "..."
     httpd.serve_forever()
 
-def main():
-    
+def qualify():
     global teams
     teams = qualifyed_teams()
+
+def main():
+    update_repos()
+    qualify()
     global GameThread
     GameThread = threading.Thread(target=play_game)
     GameThread.start()
-    
     start_web_server()
     
 if __name__=="__main__":
